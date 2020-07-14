58ec4c39f1c10c76e95e94951bddaeecf6e1b03e
Ergina Kamilia <erginakamiliaputri@gmail.com>
2020-07-14 15:10:09 +0700

    project pertama

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+if (workbox)
+	console.log(`Workbox berhasil dimuat`);
+else
+	console.log(`Workbox gagal dimuat`);
+
+workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/script.js', revision: '1' },
+]);
+
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
